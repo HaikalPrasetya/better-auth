8ee448843d5a92350c989245c1757fb464459110
Haikal <kokhaikalberak12@gmail.com>
2025-08-20 11:53:04 +0700

    add nextCookie plugin, end chapter 2

--- src/app/actions/sign-in-email-action.ts
+++ src/app/actions/sign-in-email-action.ts
@@ -12,34 +12,13 @@ export async function signInEmailAction(formData: FormData) {
   if (!password) return { error: "Please enter your password" };
 
   try {
-    const res = await auth.api.signInEmail({
+    await auth.api.signInEmail({
       headers: await headers(),
       body: {
         email,
         password,
       },
-      asResponse: true,
     });
-
-    const setCookieHeader = res.headers.get("set-cookie");
-    if (setCookieHeader) {
-      const cookie = parseSetCookieHeader(setCookieHeader);
-      const cookieStore = await cookies();
-
-      const [key, cookieAttributes] = [...cookie.entries()][0];
-      const value = cookieAttributes["value"];
-      const maxAge = cookieAttributes["max-age"];
-      const path = cookieAttributes["path"];
-      const httpOnly = cookieAttributes["httponly"];
-      const sameSite = cookieAttributes["samesite"];
-
-      cookieStore.set(key, decodeURIComponent(value), {
-        maxAge,
-        path,
-        httpOnly,
-        sameSite,
-      });
-    }
 
     return { error: null };
   } catch (error) {
